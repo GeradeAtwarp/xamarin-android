7609490f3be86c611659670b65de0b5e987f3b39
Gerade <Geldenhuys>
2017-02-15 20:03:06 +0200

    -initial commit -added navigation drawer menu

---
+++ xamarin-android/xamarin-android/MainActivity.cs
@@ -0,0 +1,59 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Support.V7.Widget;
+using Android.Support.Design.Widget;
+using Android.Support.V4.Widget;
+
+namespace xamarin_android
+{
+    [Activity(Label = "Android Worker", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : AppCompatActivity
+    {
+        DrawerLayout drawerLayout;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            
+            SetContentView (Resource.Layout.Main);
+            drawerLayout = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+
+            var toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            // Attach item selected handler to navigation view
+            var navigationView = FindViewById<NavigationView>(Resource.Id.nav_view);
+            navigationView.NavigationItemSelected += NavigationView_NavigationItemSelected;
+
+            // Create ActionBarDrawerToggle button and add it to the toolbar
+            var drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, Resource.String.open_drawer, Resource.String.close_drawer);
+            drawerLayout.SetDrawerListener(drawerToggle);
+            drawerToggle.SyncState();
+        }
+
+        private void NavigationView_NavigationItemSelected(object sender, NavigationView.NavigationItemSelectedEventArgs e)
+        {
+            switch (e.MenuItem.ItemId)
+            {
+                case (Resource.Id.nav_home):
+                    // React on 'Home' selection
+                    break;
+                case (Resource.Id.nav_messages):
+                    // React on 'Messages' selection
+                    break;
+                case (Resource.Id.nav_friends):
+                    // React on 'Friends' selection
+                    break;
+                case (Resource.Id.nav_discussion):
+                    // React on 'Discussion' selection
+                    break;
+            }
+
+            // Close drawer
+            drawerLayout.CloseDrawers();
+        }
+    }
+}
+
